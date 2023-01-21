18c6ec7364c007b9727fa7f09a28933cd99e7413
Ricardo <franciscorvpinheiro49@gmail.com>
2023-01-21 11:21:16 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
+export default function App() {
+
+  const currentDate = new Date().toLocaleDateString("pt-br")
+
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: currentDate,
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: currentDate },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: currentDate,
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: currentDate,
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      /> */}
+      {expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
-
-export default App;
